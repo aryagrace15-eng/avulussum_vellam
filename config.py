221021f1e32464325653c4429ac68ad7e8f45d32
arya <aryagrace15@gmail.com>
2026-09-03 19:41:18 +0530

    third commit

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("Bimport osOT_TOKEN")
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN not found! Add it to your .env file.")
